74a635a566e26bf3e5985b283e89efd366842fe7
Swathi-prasanna <swathi@paithacs.in>
2026-07-11 10:46:45 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
     <div className="app-layout">
       <PatientSidebar />
       <div className="content">
+        <PatientNavbar />
         <Routes>
           <Route path="/" element={<DashboardPage />} />
           <Route path="/PrescriptionPage" element={<Appointment />} />
